5ea798a2e600b660206d80df92be2dd04daaaa7c
Nicole Dodd <nicolechdodd@gmail.com>
2020-07-20 15:00:18 -0700

    Replace '\n' with space
    Added a space where '\n' was removed.
    Previously, 'de\nla' became 'dela'. Now, 'de\nla' becomes 'de la'.

--- cleanNewlines.py
+++ cleanNewlines.py
@@ -6,7 +6,7 @@ def removeNewlines(file):
     f = open(file, 'r', encoding="utf-8")
     contents = f.read()
     f.close()
-    new_contents = contents.replace('\n', '')
+    new_contents = contents.replace('\n', ' ')
     f = open(file, 'w', encoding="utf-8")
     f.write(new_contents)
     f.close()
